aa81e4c85e55ef12d1afaed2dac2051a0386442d
Meash <m4rtinsch@gmail.com>
2015-09-13 00:46:09 +0200

    Set up multisite

--- wp-config.php
+++ wp-config.php
@@ -81,6 +81,12 @@ define('WP_DEBUG', false);
 
 /* Multisite */
 define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'localhost');
+define('PATH_CURRENT_SITE', '/wordpress/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /* That's all, stop editing! Happy blogging. */
 
