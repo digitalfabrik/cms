238570b47ef014ca2dd58d9ef67fe19b2cc0491b
Martin Schrimpf <martin.schrimpf@outlook.com>
2015-09-15 07:35:14 +0200

    Change multisite domain from localhost to the server domain

--- wp-config.php
+++ wp-config.php
@@ -83,7 +83,7 @@ define('WP_DEBUG', false);
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'localhost');
+define('DOMAIN_CURRENT_SITE', 'vmkrcmar21.informatik.tu-muenchen.de');
 define('PATH_CURRENT_SITE', '/wordpress/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
