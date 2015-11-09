2db5ee3e8b5535a876db6519219d3e5a253f0731
Meash <m4rtinsch@gmail.com>
2015-11-09 18:53:27 +0100

    Display Duesseldorf

--- wp-content/plugins/wp-api-extensions/endpoints/RestApi_Multisites.php
+++ wp-content/plugins/wp-api-extensions/endpoints/RestApi_Multisites.php
@@ -10,6 +10,7 @@ class RestApi_Multisites extends RestApi_ExtensionBase {
 
 	private $INCLUDED_SITE_IDS = [
 			2, // Augsburg
+			8, // Duesseldorf
 			15, // Main-Taunus-Kreis
 	];
 	private $GLOBAL_SITE_IDS = [5];
