5356149efaea4f824d0fd796a39dd0e096141ea3
Meash <m4rtinsch@gmail.com>
2015-12-05 17:28:00 +0100

    Return live flag, remove global flag
    resolves #242

--- wp-content/plugins/wp-api-extensions/endpoints/RestApi_Multisites.php
+++ wp-content/plugins/wp-api-extensions/endpoints/RestApi_Multisites.php
@@ -9,7 +9,7 @@ class RestApi_Multisites extends RestApi_ExtensionBase {
 	const URL = 'multisites';
 	const LIVEINSTANCES_FILE = __DIR__ . '/liveinstances.txt';
 
-	private $GLOBAL_SITE_IDS = [5];
+	private $included_site_ids = false;
 
 	public function register_routes($namespace) {
 		parent::register_route($namespace,
@@ -20,13 +20,10 @@ class RestApi_Multisites extends RestApi_ExtensionBase {
 
 	public function get_multisites() {
 		$multisites = wp_get_sites();
-		$included_site_ids = $this->get_live_instances();
+		$this->load_included_site_ids();
 
 		$result = [];
 		foreach ($multisites as $blog) {
-			if (!in_array($blog['blog_id'], $included_site_ids)) {
-				continue;
-			}
 			$result[] = $this->prepare_item($blog);
 		}
 		return $result;
@@ -44,7 +41,7 @@ class RestApi_Multisites extends RestApi_ExtensionBase {
 			'color' => '#FFA000',
 			'path' => $blog['path'],
 			'description' => get_bloginfo($blog),
-			'global' => in_array($id, $this->GLOBAL_SITE_IDS)
+			'live' => in_array($id, $this->included_site_ids)
 		];
 		restore_current_blog();
 		return $result;
@@ -70,6 +67,18 @@ class RestApi_Multisites extends RestApi_ExtensionBase {
 			fclose($handle);
 		}
 		return $ids;
+	}
+
+	/**
+	 * If the included site ids are not already loaded,
+	 * retrieves them and stores them in #included_site_ids.
+	 */
+	private function load_included_site_ids() {
+		if ($this->included_site_ids !== false) {
+			// already loaded
+			return;
+		}
+		$this->included_site_ids = $this->get_live_instances();
 	}
 }
 
