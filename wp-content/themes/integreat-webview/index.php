0b00f0ec6c3b817c3abf0a36edeeb8e16c602a96
sascha11110 <sascha.beele123@gmail.com>
2016-03-07 23:53:50 +0100

    integreat-webview-themes

---
+++ wp-content/themes/integreat-webview/index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+    <?php get_template_part('templates/contentHeader'); ?>
+
+    <main>
+        <div class="container">
+            <?php if( $_GET['isite'] == event ): ?>
+                <?php echo do_shortcode('[events_list]'); ?>
+            <?php elseif( is_front_page() ): ?>
+                <?php include get_template_directory() . '/templates/frontPage.php'; ?>
+            <?php else: ?>
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <section class="content searchOnSiteContent">
+                        <?php the_content(); ?>
+                    </section>
+                <?php endwhile; endif; ?>
+            <?php endif; ?>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
