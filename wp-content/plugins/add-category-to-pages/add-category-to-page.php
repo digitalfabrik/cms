3c051a2f7a3b46d2aa7da6740d40e37bc649d0b2
sven <sven@geeq.de>
2016-05-25 13:26:00 +0200

    only tags for pages, not categories

--- wp-content/plugins/add-category-to-pages/add-category-to-page.php
+++ wp-content/plugins/add-category-to-pages/add-category-to-page.php
@@ -12,7 +12,7 @@
  
  function add_taxonomies_to_pages() {
       register_taxonomy_for_object_type( 'post_tag', 'page' );
-      register_taxonomy_for_object_type( 'category', 'page' );
+      //register_taxonomy_for_object_type( 'category', 'page' );
   } 
 
  add_action( 'init', 'add_taxonomies_to_pages' );
