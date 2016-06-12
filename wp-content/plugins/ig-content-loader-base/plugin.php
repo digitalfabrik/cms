f9fc011389660c9b99902da39aa3d635074159d6
Julian Orth <julian1.orth@stud-uni.regensburg.de>
2016-06-12 15:44:33 +0200

    get plugin-list, save selected plugin to meta
    -built dropdown-select from plugins
    -save selected plugin in post meta

--- wp-content/plugins/ig-content-loader-base/plugin.php
+++ wp-content/plugins/ig-content-loader-base/plugin.php
@@ -94,6 +94,8 @@ function cl_generate_select_list() {
  */
 function cl_generate_selection_box() {
     add_meta_box( 'meta-box-id', __( 'Fremdinhalte einfügen', 'textdomain' ), 'cl_my_display_callback', 'page', 'side' );
+    
+    
 }
 add_action( 'add_meta_boxes_page', 'cl_generate_selection_box' );
 //add_action( 'add_meta_boxes_page', 'cl_generate_select_list' );
@@ -109,32 +111,10 @@ function cl_my_display_callback( $post ) {
     $prfx_stored_meta = get_post_meta( $post->ID );
     
 
-    // get all plugins and store them in array
-    $cl_plugin_stack = array();
-    $cl_plugin_names = array();
-    $cl_plugins = get_plugins();
-    foreach ( $cl_plugins as $cl_plugin ) {
-        array_push($cl_plugin_stack, $cl_plugin);
-    }
-    
-    //loop through plugin array and get all plugin names
-    for ($x = 0; $x < count($cl_plugin_stack); $x++) {  
-      array_push($cl_plugin_names, $cl_plugin_stack[$x][Name]);    
-    }
-    //debug_to_console($cl_plugin_names);
-    
-    $cl_matches = preg_grep('/Content Loader/', $cl_plugin_names); 
-    //$cl_keys    = array_keys($cl_matches); 
-    
-    debug_to_console($cl_matches);
-
-    
+    $dropdown_items = apply_filters('cl_metabox_item', $array);
+  
 
 ?>
-
-    
-
-
 
 
     <!-- foreign content: Dropdown-select -->
@@ -142,11 +122,13 @@ function cl_my_display_callback( $post ) {
         <label style="font-weight:600" for="meta-select" class="prfx-row-title">
             <?php _e( 'Inhalt wählen', 'prfx-textdomain' )?>
         </label>
-        <select name="meta-select" id="meta-select" style="width:100%; margin-top:10px; margin-bottom:10px">
+        <select name="cl_content_select2" id="meta-select" style="width:100%; margin-top:10px; margin-bottom:10px">
             <!-- build select items from filtered plugin list -->
+            
             <?php 
-                foreach($cl_matches as $cl_plugin_name_option) {
-                    print('<option value="'.$cl_plugin_name_option.'">'.$cl_plugin_name_option.'</option>'."\n");  
+                foreach($dropdown_items as $cl_plugin_name_option) {
+//                    print('<option name="cl_content_select" value="'.$cl_plugin_name_option[id].'">'.$cl_plugin_name_option[id].'</option>'."\n");  
+                    print('<option name="cl_content_select">'.$cl_plugin_name_option->name.'</option>');
                 }
             ?>
             
@@ -176,27 +158,106 @@ function cl_my_display_callback( $post ) {
         <input style="width:100%;" name="loadandinsert" type="submit" class="button button-primary button-large" id="s" value="Einfügen">
     </div>
 
+   
     <?php
+       
+   
 }
  
+/* Save post meta on the 'save_post' hook. */
+  //add_action( 'pre_post_update', 'smashing_save_post_class_meta', 10, 2 );
+
+/* Save the meta box's post metadata. */
+function smashing_save_post_class_meta( $post_id, $post ) {
+
+  /* Verify the nonce before proceeding. */
+//  if ( !isset( $_POST['smashing_post_class_nonce'] ) || !wp_verify_nonce( $_POST['smashing_post_class_nonce'], basename( __FILE__ ) ) )
+//    return $post_id;
+    debug_to_console("ok");
+
+  /* Get the post type object. */
+  $post_type = get_post_type_object( $post->post_type );
+
+  /* Check if the current user has permission to edit the post. */
+  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+    return $post_id;
+
+  /* Get the posted data and sanitize it for use as an HTML class. */
+  $new_meta_value = ( isset( $_POST['cl_content_select'] ) ? sanitize_html_class( $_POST['cl_content_select'] ) : '' );
+
+  /* Get the meta key. */
+  $meta_key = 'smashing_post_class';
+
+  /* Get the meta value of the custom field key. */
+  $meta_value = get_post_meta( $post_id, $meta_key, true );
+
+  /* If a new meta value was added and there was no previous value, add it. */
+  if ( $new_meta_value && '' == $meta_value )
+    add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+    
+  /* If the new meta value does not match the old value, update it. */
+  elseif ( $new_meta_value && $new_meta_value != $meta_value )
+    update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+  /* If there is no new meta value but an old value exists, delete it. */
+  elseif ( '' == $new_meta_value && $meta_value )
+    delete_post_meta( $post_id, $meta_key, $meta_value );
+}
+
+
+
+//add_action( 'save_post', 'cl_save_meta_box' );
+
 /**
  * Save meta box content.
  *
  * @param int $post_id Post ID
  */
-function cl_save_meta_box( $post_id ) {
+add_action('save_post', 'cl_save_meta_box');
+add_action('edit_post', 'cl_save_meta_box');
+add_action('publish_post', 'cl_save_meta_box');
+add_action('edit_page_form', 'cl_save_meta_box');
+//save in postmeta
+function cl_save_meta_box($post_id) {
     // Save logic goes here. Don't forget to include nonce checks!
 	//wenn element aus cl_generate_selection_box ausgewählt wurde, irgendwie in postmeta speichern
-	$cl_content = $_GET['cl_content'];
-	
-	//save in postmeta
-}
-add_action( 'save_page', 'cl_save_meta_box' );
+	//$cl_content = $_GET['cl_content_select'];
+    /* Get the meta key. */
+    $meta_key = 'ig-content-loader-base';
+    $meta_value = $_REQUEST['cl_content_select2'];
+//    var_dump("leck michaksldjfasjkldfjklasdfjkasdjkfjaklsdfkljasdjfklalsjkdfjlkasdfkljasdlkjf");
+    var_dump($meta_value);
+    
+//    var_dump($_POST);
+    if ($meta_value) {
+        
+    /* Get the meta value of the custom field key. */
+        $meta_value = get_post_meta( $post_id, $meta_key, true );
 
-function cl_save_content() {
-	do_action('cl_save_content');
+    /* If a new meta value was added and there was no previous value, add it. */
+    if ( $new_meta_value && '' == $meta_value )
+        add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+
+    /* If the new meta value does not match the old value, update it. */
+    elseif ( $new_meta_value && $new_meta_value != $meta_value )
+        update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+    /* If there is no new meta value but an old value exists, delete it. */
+    elseif ( '' == $new_meta_value && $meta_value )
+        delete_post_meta( $post_id, $meta_key, $meta_value );
+
+    }
+}
+   
 	
-	save_post($posttype='attachment',$title,$content,$parent_id);
+	
+
+
+// wird aufgerufen mit id und html code, welcher als attach gespeichert wird
+function cl_save_content( $parent_id, $attachement) {
+	// noch mal prüfen:
+	save_post($posttype='attachment',$title,$attachement,$parent_id);
+    
 	// eigene datenstruktur oder wp_posts und eigenen datentypen definieren bzw attachment(!!!) benutzen?
 }
 
@@ -211,12 +272,17 @@ add_action('rest_api_print_post', 'modify_post', 1);
 // do_action in der rest api: bei ausgabe von posts muss bei entsprechendem meta tag ein do_action('rest_api_print_post') aufgerufen werden
 function cl_update () {
     global $wp_query;
+    global $wpdb;
+    // query alle objekte in db mit meta_key = ig-content-loader-base
+    $result = $wpdb->get_results("select post_id from ".$wpdb->prefix."_postmeta where meta_key = 'ig-content-loader-base'");
+    
     // wird regelmäßig durch cronjob gestartet
+    // parse var content-loader aus url
     $cl_action = $wp_query->query_vars['content-loader'];
     
     if( $cl_action == "update" ) {
         
-        do_action('cl_update_content');
+        do_action('cl_update_content', $parent_id, $meta_value );
         
         exit();
     }
@@ -227,7 +293,7 @@ add_action( 'template_redirect', 'cl_update' );
 // do_action in der rest api: bei ausgabe von posts muss bei entsprechendem meta tag ein do_action('rest_api_print_post') aufgerufen werden
 
 function cl_rewrite() {
-
+    // es gibt var content-loader
     add_rewrite_tag( '%content-loader%', '([^&]+)' );
     //add_rewrite_rule( 'content-loader/([^/]*)/?', 'index.php?content-loader=$matches[1]', 'top');
 
--- wp-content/plugins/ig-content-loader-sprungbrett/plugin.php
+++ wp-content/plugins/ig-content-loader-sprungbrett/plugin.php
@@ -9,14 +9,29 @@
  * License: MIT
  */
 
-function cl_sb_update_content() {
+function cl_sb_update_content( $parent_id, $meta_value) {
 	// get stuff from sprungbrett api
+    if($meta_value == "ig-content-loader-sprungbrett") {
 	$json = file_get_contents('http://localhost/json.txt');
     $json = json_decode($json);
-    
-    var_dump($json);
+    $html = cl_sb_jsontohtml($json);
+    cl_save_content( $parent_id, $html );
+        
+    }
 
 }
 add_action('cl_update_content','cl_sb_update_content');
 
 
+function cl_sb_jsontohtml($json) {
+    // aus json html liste erstellen
+    $html ="<p>testcontent</p>";
+    return $html;
+}
+
+// registriert plugin in base and return meta infos
+function cl_sb_metabox_item($array) {
+    $array[] = json_decode('{"id": "ig-content-loader-sprungbrett", "name": "Sprungbrett Praktika"}');
+    return $array;
+}
+add_filter('cl_metabox_item', 'cl_sb_metabox_item');
