d8e7dec6d993de19defc03d080ee6741feed7a13
sven <sven@geeq.de>
2016-06-17 18:18:30 +0200

    removed old functions fromg sprungbrett, left over from initial brainstorming

--- wp-content/plugins/ig-content-loader-base/plugin.php
+++ wp-content/plugins/ig-content-loader-base/plugin.php
@@ -8,50 +8,6 @@
  * License: MIT
  */
 
-global $cl_template_db_version;
-$cl_template_db_version = '1.0';
-
-function cl_template_install() {
-	global $wpdb;
-	global $cl_template_db_version;
-
-	$table_name = $wpdb->prefix . '_template_content';
-	
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-		id mediumint(9) NOT NULL AUTO_INCREMENT,
-		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-		name tinytext NOT NULL,
-		text text NOT NULL,
-		url varchar(55) DEFAULT '' NOT NULL,
-		UNIQUE KEY id (id)
-	) $charset_collate;";
-
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $sql );
-
-	add_option( 'cl_template_db_version', $cl_template_db_version );
-}
-
-function jal_install_data() {
-	global $wpdb;
-	
-	$welcome_name = 'Mr. WordPress';
-	$welcome_text = 'Congratulations, you just completed the installation!';
-	
-	$table_name = $wpdb->prefix . 'liveshoutbox';
-	
-	$wpdb->insert( 
-		$table_name, 
-		array( 
-			'time' => current_time( 'mysql' ), 
-			'name' => $welcome_name, 
-			'text' => $welcome_text, 
-		) 
-	);
-}
-
 /**
  *  Register Meta box Hook
  */
