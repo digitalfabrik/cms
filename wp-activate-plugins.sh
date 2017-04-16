0ddc09612cc8c8eed8fee89bb054c7c8248e2c49
Sven Seeberg <sven@geeq.de>
2017-04-16 15:59:04 +0200

    updated plugin activation

--- wp-activate-plugins.sh
+++ wp-activate-plugins.sh
@@ -1,38 +1,46 @@
-#!/usr/bin/env bash
+#! \usr \bin \env bash
 "vendor/wp-cli/wp-cli/bin/wp" plugin activate --network \
-  adminimize \
-  advanced-custom-fields \
-  block-specific-plugin-updates \
-  cms-tree-page-view \
-  sitepress-multilingual-cms \
-  wpml-media \
-  wpml-translation-management \
-  events-manager \
-  events-manager-wpml \
-  multisite-clone-duplicator \
-  pdf-creator-lite \
-  revisionary \
-  simple-page-ordering \
-  tinymce-advanced \
-  use-google-libraries \
-  user-role-editor \
-  wp-gcm \
-  wp-rtl \
-  ig-automatic-translation-button \
-  ig-click-guide \
-  ig-cms-tree-page-view-extensions \
-  ig-customize-login-integreat \
-  ig-editor-next-button \
-  ig-enhanced-help-menu \
-  ig-login-redirect-pages \
-  ig-mobile-preview \
-  ig-pdf-creator-lite-font-extension \
-  ig-project-user-roles \
-  ig-separate-disclaimer \
-  ig-wp-api-extensions \
-  ig-remove-403-forbidden \
-  ig-content-loader-base \
-  ig-content-loader-instance \
-  ig-content-loader-sprungbrett \
-  ig-customize-editor-integreat \
-  ig-plugin-adjustment
+adminimize \
+advanced-custom-fields \
+block-specific-plugin-updates \
+cms-tree-page-view \
+disable-new-user-notifications \
+events-manager \
+events-manager-wpml \
+ig-automatic-translation \
+ig-automatic-translation-button \
+ig-click-guide \
+ig-cms-tree-page-view-extensions \
+ig-content-loader-base \
+ig-content-loader-instance \
+ig-customize-editor-integreat \
+ig-customize-login-integreat \
+ig-editor-next-button \
+ig-enhanced-help-menu \
+ig-extras \
+ig-login-redirect-pages \
+ig-menu-help \
+ig-mobile-preview \
+ig-pdf-creator-lite-font-extension \
+ig-plugin-adjustment \
+ig-project-user-roles \
+ig-remove-403-forbidden \
+ig-separate-disclaimer \
+ig-simplify-ui \
+ig-text-dir \
+ig-wp-api-extensions \
+ig-wpml-lock-media-checkboxes \
+multisite-clone-duplicator \
+pdf-creator-lite \
+revisionary \
+simple-page-ordering \
+sitepress-multilingual-cms \
+tinymce-advanced \
+use-google-libraries \
+user-role-editor \
+wpml-media \
+wpml-translation-management \
+wp-rtl \
+wp-sync-db-1.5
+
+
