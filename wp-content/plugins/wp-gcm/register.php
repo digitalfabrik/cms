259edfe99e325983e1ba424dfc9bcc6fe5af5954
Christopher van Dun <vandun@integreat-app.de>
2017-04-18 16:09:29 +0200

    Firebase Plugin added (wp-gcm). Sending functionality still missing.

---
+++ wp-content/plugins/wp-gcm/register.php
@@ -0,0 +1,25 @@
+<?php
+
+function px_gcm_register() {
+	if (!isset($_GET["gcm_register_id"])) {
+		return;
+	}
+
+	global $wpdb;
+	$gcm_regid = $_GET["gcm_register_id"];
+	$px_table_name = $wpdb->prefix . 'gcm_users';
+	$query = "SELECT gcm_regid FROM $px_table_name WHERE gcm_regid='$gcm_regid'";
+	$result = $wpdb->get_results($query);
+
+	if ($result) {
+		echo "You are already registered on blog " . get_current_blog_id();
+	} else {
+		$query = "INSERT INTO $px_table_name (gcm_regid, created_at) VALUES ('$gcm_regid', 'NOW()')";
+		if ($wpdb->query($query) === false) {
+			throw new RuntimeException("Could not insert into GCM registration table $px_table_name: "
+				. $wpdb->last_error);
+		}
+		echo "You are now registered on blog " . get_current_blog_id();
+	}
+	exit;
+}
