70ce7dda73c59c0bc94b1de155c1ead5ffcc17b4
Sven Seeberg <sven@geeq.de>
2017-08-25 18:21:38 +0200

    preview only on page load, fixes #550 (#553)

--- wp-content/plugins/ig-mobile-preview/js/copy-content.js
+++ wp-content/plugins/ig-mobile-preview/js/copy-content.js
@@ -10,15 +10,15 @@
 		mobilePreview.title.html(titleContent);
 	};
 
-	/**
-	 * Copy text from the text editor
-	 */
 	mobilePreview.copyTextContent = function () {
 		mobilePreviewWPAutoP(jQuery("#content").html());
 	};
 }(window.mobilePreview = window.mobilePreview || {}, jQuery));
 
 function mobilePreviewWPAutoP( editorContent ){
+	if(editorContent == "") {
+		return;
+	}
 	jQuery.post( "index.php", { 'mpvwpautop': editorContent })
 	.done(function( data ) {
 		mobilePreview.content.html( data );
@@ -32,19 +32,6 @@ jQuery(document).ready(function () {
 	mobilePreview.title = jQuery('#mobile-preview-title');
 	mobilePreview.content = jQuery('#mobile-preview-content');
 
-	mobilePreview.postTitle.on("keyup", mobilePreview.copyTitle);
 	mobilePreview.copyTitle();
-
-	mobilePreview.postContent.on("keyup", mobilePreview.copyTextContent);
 	mobilePreview.copyTextContent();
 });
-
-
-/**
- * Copy text from the tinyMCE editor
- * @param editor the tinyMCE editor
- */
-function mobilePreviewCopyTinymceContent(editor) {
-	var editorContent = editor.getContent();
-	mobilePreview.content.html(editorContent);
-}
