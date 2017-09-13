7ce7799dece15cd6477fa19ac3bdbe078cf177a6
Sven Seeberg <sven@geeq.de>
2017-09-13 16:50:16 +0200

    API empty string for all pages with children, fixes #557

--- wp-content/plugins/ig-wp-api-extensions/endpoints/v0/RestApi_ModifiedContent.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/v0/RestApi_ModifiedContent.php
@@ -214,7 +214,11 @@ abstract class RestApi_ModifiedContentV0 extends RestApi_ExtensionBaseV0 {
 	}
 
 	protected function prepare_content($post) {
-		$content = ( $post->post_content == "" ? "empty" : $post->post_content );
+
+		$children = get_pages( array( 'child_of' => $post->ID ) );
+		if( count( $children ) == 0 ) {
+			$content = ( $post->post_content == "" ? "empty" : $post->post_content );
+		}
 		return wpautop($content);
 	}
 
