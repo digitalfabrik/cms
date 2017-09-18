d7852c6ae42fc3cb604d98cfcef03a91d019c2d8
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-09-18 12:55:39 +0200

    Text allowed for pages having children

--- wp-content/plugins/ig-wp-api-extensions/endpoints/v0/RestApi_ModifiedContent.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/v0/RestApi_ModifiedContent.php
@@ -215,8 +215,10 @@ abstract class RestApi_ModifiedContentV0 extends RestApi_ExtensionBaseV0 {
 
 	protected function prepare_content($post) {
 		$children = get_pages( array( 'child_of' => $post->ID ) );
-		if( count( $children ) == 0 ) {
-			$content = ( $post->post_content == "" ? "empty" : $post->post_content );
+		if( "" == $post->post_content &&count( $children ) == 0 ) {
+			$content = "empty";
+		} else {
+			$content = $post->post_content;
 		}
 		return wpautop($content);
 	}
