5eb468dd1a8de2dcec9e85ce645e064c8e487b27
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-09-23 22:46:22 +0200

    Creating events works

--- wp-content/plugins/ig-nuernberg-calendar/em-event-wrapper.php
+++ wp-content/plugins/ig-nuernberg-calendar/em-event-wrapper.php
@@ -1,20 +1,37 @@
 <?php
 
+function compute_number_events() {
+
+}
+
+function parse_oeffnungszeiten_type1() {
+
+}
+
+function parse_oeffnungszeiten_type2() {
+
+}
+
+function parse_oeffnungszeiten_type3() {
+
+}
+
+
 class IG_NUE_Event extends EM_Event {
 	var $ig_nue_source_id;
 
 
 	function import_xml_data( $xml_element ) {
-		$this->ig_nue_source_id;
-		$this->event_slug;
-		$this->event_owner;
-		$this->event_name;
+		$this->ig_nue_source_id = (int) $xml_element['ID'];
+		// $this->event_slug;
+		$this->event_owner = get_current_user_id();
+		$this->event_name = (string) $xml_element->TITEL;
 		$this->event_start_time;
 		$this->event_end_time;
 		$this->event_all_day;
 		$this->event_start_date;
 		$this->event_end_date;
-		$this->post_content;
+		$this->post_content = (string) $xml_element->UNTERTITEL + "<br><a href='" + (string) $xml_element->DETAILLINK + "'>" + (string) $xml_element->DETAILLINK + "</a>";
 		$this->event_rsvp;
 		$this->event_rsvp_date;
 		$this->event_rsvp_time = "00:00:00";
@@ -37,13 +54,30 @@ class IG_NUE_Event extends EM_Event {
 		$this->event_owner_anonymous;
 		$this->event_owner_name;
 		$this->event_owner_email;
-		echo "<p>";
-		var_dump( (string)$event->ORT );
-		echo "</p>";
+		$this->location = new EM_Location;
+		
+		$this->location->location_name = (string) $xml_element->ORT;
 	}
 
 	function save_nue_event () {
-		
+		//var_dump($this);
+
+		/**
+		 * return, we do not want to spam the database during testing (yet)
+		**/
+		//return true;
+
+		/**
+		 * Use the EM saving function. But we need to create an additional post meta with the source ID.
+		**/
+		//$save_location_return = $this->location->save();
+		$this->location_id = $this->location->id;
+		echo "<p>Location: $save_location_return</p>";
+		//$save_return = $this->save();
+		echo "<p>Event: $save_return</p>";
+		if( $this->post_id ) {
+			
+		}
 	}
 }
 
--- wp-content/plugins/ig-nuernberg-calendar/plugin.php
+++ wp-content/plugins/ig-nuernberg-calendar/plugin.php
@@ -10,6 +10,7 @@
 
 
 require_once("sort-events.php");
+require_once("em-event-wrapper.php");
 register_activation_hook(__FILE__, 'ig_ncal_activation');
 
 function ig_ncal_activation() {
@@ -17,25 +18,40 @@ function ig_ncal_activation() {
 		//wp_schedule_event(time(), 'daily', 'ig_ncal_import_event');
 	}
 }
-	
+
+
+/*
+ * The ig_ncal_import() function is usually called by a WP cron job.
+ * It fetches the data from the Nuremberg region event calendar and stores all events as Event Manager event posts.
+ * The source ID is saved as a meta value. If the source ID is already stored, the event will not be processed again.
+ */
 add_action('ig_ncal_import_event', 'ig_ncal_import');
 function ig_ncal_import() {
+	/*
+	 * Get data from API and parse XML
+	 */
 	$cal_xml_file = file_get_contents('https://www.meine-veranstaltungen.net/export.php5');
 	$events = new SimpleXMLElement($cal_xml_file);
+
+
 	foreach( $events as $event ) {
 		$post = get_posts( array(
 			'meta_key'   => 'ncal_event_id',
 			'meta_value' => $events['ID'],
 		) );
-		if( count( $post ) == 0 ) {
+		if( count( $post ) > 0 ) {
 			/* 
 			 * Event already stored, continue with next event
 			 */
 			continue;
 		}
+
+		/*
+		 * Create a new event, import XML data and save
+		 */
 		$newEMEvent = new IG_NUE_Event;
 		$newEMEvent->import_xml_data( $event );
-		$newEMevent->save_nue_event();
+		$newEMEvent->save_nue_event();
 		unset( $newEMEvent );
 	}
 }
