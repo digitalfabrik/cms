f663bf666b0c322810a147f25c43b127e1fdbc1f
Sven Seeberg <sven@geeq.de>
2017-10-22 18:06:45 +0200

    Import Nuremberg calendar, fixes #533 (#563)
    enables importing the calendar of the Nuremberg region, available at https://www.meine-veranstaltungen.net/export.php5

---
+++ wp-content/plugins/ig-nuernberg-calendar/sort-events.php
@@ -0,0 +1,16 @@
+<?php
+
+function ig_ncal_menu() {
+		add_menu_page( 'Calendar Import', 'Calendar Import', 'edit_events', 'ig_ncal_edit', 'ig_ncal_sort_events', 'dashicons-clock', $position = 99 );
+}
+add_action( 'admin_menu', 'ig_ncal_menu' );
+
+function ig_ncal_man_import() {
+
+}
+
+function ig_ncal_sort_events() {
+	ig_ncal_import();
+}
+
+?>
