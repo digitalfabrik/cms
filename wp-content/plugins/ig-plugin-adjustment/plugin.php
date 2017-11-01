dc6efe2a0748fe11ffdf0854494df90020118e8e
Sven Seeberg <sven@geeq.de>
2017-11-01 15:40:00 +0100

    removed revisionary, do not modify tree-view plugin

--- wp-content/plugins/ig-plugin-adjustment/plugin.php
+++ wp-content/plugins/ig-plugin-adjustment/plugin.php
@@ -13,17 +13,10 @@ add_action('upgrader_process_complete', function ($upgrader_object, $options) {
 	if (strcasecmp($options['type'], 'plugin') != 0) // only adjust plugins
 		return true;
 	$adjuster = new PluginAdjustment();
-	$adjuster->apply_revisionary_adjustments();
 	return true;
 }, 10, 2);
 
 class PluginAdjustment {
-	public function apply_revisionary_adjustments() {
-		$tree_view_file = plugin_dir_path(__FILE__) . '../cms-tree-page-view/functions.php';
-		$search = '"post_status": "<?php echo $onePage->post_status ?>",';
-		$replace = '"post_status": "<?php echo page_has_pending_revision($onePage->ID) ? \'pending\' : $onePage->post_status ?>",';
-		$this->replace_line_in_file($tree_view_file, $search, $replace);
-	}
 
 	private function replace_line_in_file($filepath, $search, $replace) {
 		$line_found = false;
