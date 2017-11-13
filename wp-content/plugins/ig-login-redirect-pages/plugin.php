830252a45f0eed3e2b128d95a25f37f4d2642161
Ulli <uholtgrave@googlemail.com>
2017-11-13 16:59:51 +0100

    Adding Comments and fixing typo

--- wp-content/plugins/ig-login-redirect-pages/plugin.php
+++ wp-content/plugins/ig-login-redirect-pages/plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Login redirect Pages
- * Description: Redirects all users to 'All Pages' instead of the Dashboard after login
+ * Description: Redirects all users to 'All Pages' instead of the Dashboard after login and fixes a small problem inside of the Page Editor.
  * Version: 0.1
  * Author: Martin Schrimpf
  * Author URI: https://github.com/Meash
@@ -15,10 +15,12 @@ function login_redirect_pages() {
 	$url = esc_url_raw(admin_url('edit.php?post_type=page&page=cms-tpv-page-page&action=cms_tpv_remove_promo'));
 	return $url;
 }
+
+// Fixes a translation Problem inside of the Page Editor.
 add_action('do_meta_boxes', 'replace_featured_image_box');  
 function replace_featured_image_box()  
 {  
 	remove_meta_box( 'postimagediv', 'page', 'side' );  
 	add_meta_box('postimagediv', __('Beitragsbild'), 'post_thumbnail_meta_box', 'page', 'side', 'low');  
 }  
-
+?>
