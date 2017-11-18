ac400585f170d2d0899a942a6beab844956651f5
Sven Seeberg <sven@geeq.de>
2017-11-18 21:44:40 +0100

    Fix hooks and function name for recover password domain fix, fixes #598 (#610)

--- wp-content/plugins/ig-domainfix/plugin.php
+++ wp-content/plugins/ig-domainfix/plugin.php
@@ -8,17 +8,15 @@
  * License: MIT
  */
 
-add_filter( 'wp_mail', 'if_mail_filter', 1000);
+add_filter( 'wp_mail', 'ig_mail_filter', 1000);
+add_filter( 'password_change_email', 'ig_mail_filter', 1000);
 function ig_mail_filter( $args ) {
-	
 	$new_wp_mail = array(
 		'to'          => $args['to'],
 		'subject'     => $args['subject'],
 		'message'     => str_replace("http://vmkrcmar21.informatik.tu-muenchen.de/wordpress", "https://cms.integreat-app.de", $args['message']),
 		'headers'     => $args['headers'],
-		'attachments' => $args['attachments'],
 	);
-	
 	return $new_wp_mail;
 }
 
