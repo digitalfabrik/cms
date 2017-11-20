fdc5cd29b6505c4eaaaa8b60976d994f5c508c22
Sascha Beele <sascha.beele@startmail.com>
2017-11-20 02:25:40 +0100

    Full implementation

---
+++ wp-content/plugins/ig-mpdf/plugin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Integreat mpdf
+Description: Plugin for pdf exports of single and multiple sites via mpdf library
+Author:      Sascha Beele
+ */
+
+include __DIR__ . '/IntegreatMpdf.php';
+include __DIR__ . '/IntegreatMpdfAPI.php';
+include __DIR__ . '/IntegreatMpdfSettings.php';
+
+// constant for plugin path
+if(!defined('IG_MPDF_PATH')) {
+    define('IG_MPDF_PATH', get_option('siteurl') . '/wp-content/plugins/ig-mpdf/');
+}
+
+// create database table on activation
+function ig_mpdf_install() {
+    IntegreatMpdfSettings::create_database_table();
+}
+register_activation_hook( __FILE__, 'ig_mpdf_install' );
+
+// init backend functionality and API
+function init_ig_mpdf() {
+    if(is_admin()) {
+        new IntegreatMpdfSettings();
+    }
+    new IntegreatMpdfAPI();
+}
+add_action('init', 'init_ig_mpdf');
