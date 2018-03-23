5fde9db1352072c353a824b668bd41c64448669b
Timo Ludwig <ti.ludwig@web.de>
2018-03-23 01:45:39 +0100

    [ig-sitemap] add possibility for alternate language links

--- wp-content/plugins/ig-sitemap/IntegreatSitemap.php
+++ wp-content/plugins/ig-sitemap/IntegreatSitemap.php
@@ -3,6 +3,7 @@
 class IntegreatSitemap {
 
 	const HOST = 'https://web.integreat-app.de';
+	const XHTML_ENABLED = false;
 
 	public function __construct() {
 		add_action('rest_api_init', function () {
@@ -21,7 +22,12 @@ class IntegreatSitemap {
 
 	public function get_sitemap() {
 		header('Content-Type: application/xml');
-		echo '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL.'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+		global $wpdb;
+		if (self::XHTML_ENABLED) {
+			echo '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL.'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">';
+		} else {
+			echo '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL.'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+		}
 		$pages = (new WP_Query([
 			'post_type' => ['page', 'event', 'disclaimer'],
 			'post_status' => 'publish',
@@ -31,7 +37,23 @@ class IntegreatSitemap {
 ?>
   <url>
     <loc><?= get_permalink($page) ?></loc>
-    <lastmod><?= $page->post_modified_gmt ?></lastmod>
+<?php
+			if (self::XHTML_ENABLED) {
+				$current_language = apply_filters('wpml_current_language', null);
+				$languages = $wpdb->get_col("SELECT code FROM {$wpdb->prefix}icl_languages WHERE active = 1");
+				foreach ($languages as $language) {
+					$id = apply_filters('wpml_object_id', $page->ID, $page->post_type, false, $language);
+					if (false && $id != null) {
+						do_action('wpml_switch_language', $language);
+?>
+    <xhtml:link rel="alternate" hreflang="<?= $language ?>" href="<?= get_permalink($id) ?>"/>
+<?php
+					}
+				}
+				do_action('wpml_switch_language', $current_language);
+			}
+?>
+    <lastmod><?= date('c', strtotime($page->post_modified_gmt)) ?></lastmod>
     <changefreq><?= $this->changefreq($page) ?></changefreq>
     <priority><?= $this->get_priority($page) ?></priority>
   </url>
@@ -57,13 +79,13 @@ class IntegreatSitemap {
 				$last_modified = (new WP_Query([
 					'post_type' => ['page', 'event', 'disclaimer'],
 					'post_status' => 'publish',
-					'orderby' => 'modified_gmt',
+					'orderby' => 'modified',
 					'posts_per_page' => 1,
 				]))->posts[0]->post_modified_gmt;
 ?>
   <sitemap>
     <loc><?= self::HOST.$site->path.$language.'/sitemap.xml' ?></loc>
-    <lastmod><?= $last_modified ?></lastmod>
+    <lastmod><?= date('c', strtotime($last_modified)) ?></lastmod>
   </sitemap>
 <?php
 			}
