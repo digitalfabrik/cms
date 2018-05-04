7cfeca5ffb9e7c90b6a3489cd793becd0d2fb708
Timo Ludwig <ti.ludwig@web.de>
2018-05-04 17:10:07 +0200

    [ig-mpdf] get custom.js via https (#721)

--- wp-content/plugins/ig-mpdf/plugin.php
+++ wp-content/plugins/ig-mpdf/plugin.php
@@ -16,7 +16,7 @@ include __DIR__ . '/IntegreatMpdfSettings.php';
 
 // constant for plugin path
 if(!defined('IG_MPDF_PATH')) {
-	define('IG_MPDF_PATH', get_option('siteurl') . '/wp-content/plugins/ig-mpdf/');
+	define('IG_MPDF_PATH', get_site_url(null, '/wp-content/plugins/ig-mpdf/', 'https'));
 }
 
 add_action('init', function() {
