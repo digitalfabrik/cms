2c53120d09bba9f359f5666c4f89a88ad5a8de4c
Sven Seeberg <sven@geeq.de>
2018-05-20 20:38:05 +0200

    API tracking: remove var_dumps and remove private info

--- wp-content/plugins/ig-wp-api-extensions/endpoints/helper/PiwikHelper.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/helper/PiwikHelper.php
@@ -5,21 +5,19 @@ function ig_api_page_tracking ( $call_name ) {
      * Contact Tracking server and save API hit
      */
     $token = get_blog_option(get_current_blog_id(), "wp-piwik_global-piwik_token");
-    var_dump($token);
     if ( !$token ) {
-        echo "Fallback<br>";
         $token = PIWIK_DEFAULT_AUTH_TOKEN;
         $idSite = PIWIK_DEFAULT_SITE_ID;
     } else {
         $idSite = get_blog_option(get_current_blog_id(), "wp-piwik-site_id");
     }
-    var_dump($token);
-    var_dump($idSite);
     $piwikTracker = new PiwikTracker( $idSite = $idSite );
     $piwikTracker->setTokenAuth( $token );
+
+    // Privay overrides
+    $piwikTracker->setBrowserLanguage('en');
+    $piwikTracker->setUserAgent('');
     $piwikTracker->setResolution(1, 1);
-    $piwikTracker->setLocalTime(date('H:i:s')); //HH:MM:SS
-    $piwikTracker->setUrl("https://".$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI]);
     $piwikTracker->setIp('1.1.1.1');
 
     $piwikTracker->doTrackPageView( $call_name );
