1c0080ed0bcb11cfa3b0c72bd49f6e685758f4d5
Sven Seeberg <sven@geeq.de>
2018-05-27 16:10:55 +0200

    Fix deactivation of tracking plugin

--- wp-content/plugins/ig-wp-api-extensions/endpoints/helper/PiwikHelper.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/helper/PiwikHelper.php
@@ -5,7 +5,7 @@ function ig_api_page_tracking ( $call_name ) {
      * Contact Tracking server and save API hit
      */
     $token = get_blog_option(get_current_blog_id(), "wp-piwik_global-piwik_token");
-    if ( !$token ) {
+    if ( !$token || !is_plugin_active('wp-piwik') ) {
         $token = PIWIK_DEFAULT_AUTH_TOKEN;
         $idSite = PIWIK_DEFAULT_SITE_ID;
     } else {
