728bf914bba1c48388ce47214720c5a375a405eb
Ulli Holtgrave <uholtgrave@googlemail.com>
2018-10-14 12:53:33 +0200

    Change Integreat-Help URL
    Change Url, increase version

--- wp-content/plugins/ig-menu-help/plugin.php
+++ wp-content/plugins/ig-menu-help/plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Menu Link to hilfe.integreat-app.de
  * Description: Template-base to include any foreign content into Integreat
- * Version: 1.0
+ * Version: 1.1
  * Author: Julian Orth
  * Author URI: https://github.com/Integreat
  * License: MIT
@@ -15,6 +15,6 @@ add_action('admin_menu', 'example_admin_menu');
 */
 function example_admin_menu() {
     global $submenu;
-    $url = 'https://hilfe.integreat-app.de/qa/';
+    $url = 'https://wiki.integreat-app.de/';
     $submenu['tools.php'][] = array('Integreat Hilfe', 'edit_pages', $url);
 }
