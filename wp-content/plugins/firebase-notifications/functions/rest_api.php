1542c17d56a463bb2fb51632ceda212b40bc2e34
Sven Seeberg <sven@geeq.de>
2018-10-27 15:37:30 +0200

    Change API FCM endpoint
    * Remove to parameter from endpoint URL. Instead, the
      channel name is used. The topic name will be generated
      automatically.
    * Channel can be empty. Then all messages will be
      returned.

--- wp-content/plugins/firebase-notifications/functions/rest_api.php
+++ wp-content/plugins/firebase-notifications/functions/rest_api.php
@@ -6,14 +6,14 @@
  * @param string $to filter by topic
  * @return array
  */
-function fcm_rest_api_messages ( $language, $to ) {
+function fcm_rest_api_messages ( $language, $to = Null ) {
     $fcmdb = New FirebaseNotificationsDatabase();
     $newer_than = date('Y-m-d G:i:s', time() - ( 2 * 7 * 24 * 3600 ));
     $messages = $fcmdb->messages_by_language( $lang = $language, $amount = 0, $timestamp = $newer_than );
     $return = array();
     $n = 0;
     foreach ( $messages as $message ) {
-        if ( $message['answer'] != Null And $message['request']['to'] == "/topics/" . $to ) {
+        if ( $message['answer'] != Null && ( $to === Null || $message['request']['to'] == "/topics/" . $to ) ) {
             $return[$n]['timestap'] = $message['timestamp'];
             $return[$n]['title'] = $message['request']['notification']['title'];
             $return[$n]['message'] = $message['request']['notification']['body'];
--- wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_FCM.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_FCM.php
@@ -6,7 +6,12 @@ class APIv3_FCM extends APIv3_Base_Abstract {
 
 	public function get_fcm() {
 		if (class_exists('FirebaseNotificationsDatabase')) {
-			return fcm_rest_api_messages ( ICL_LANGUAGE_CODE, $_GET['to'] );
+			if ( $_GET['channel'] === Null ) {
+				$to = Null;
+			} else {
+				$to = get_current_blog_id() . '-' . ICL_LANGUAGE_CODE . '-' . $_GET['channel'];
+			}
+			return fcm_rest_api_messages ( ICL_LANGUAGE_CODE, $to );
 		} else {
 			// throw error if IntegreatSettingsPlugin is not activated
 			return new WP_Error('settings_plugin_not_activated', 'The Plugin "Integreat Settings" is not activated for this location', ['status' => 501]);
