c73849b80a3c9b667ef4ef2999f44acbe87b52d1
Timo Ludwig <ti.ludwig@web.de>
2018-10-31 15:00:19 +0100

    [debug] enable debug log

--- wp-config.php
+++ wp-config.php
@@ -80,9 +80,11 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 // Enable Debug logging to the /wp-content/debug.log file
 define('WP_DEBUG_LOG', true);
+// Disable display of errors and warnings
+define('WP_DEBUG_DISPLAY', false);
 
 /* Trash */
 define('EMPTY_TRASH_DAYS', 99999); // "never" delete content
