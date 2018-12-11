5ad258b0e426398f4ec60f45127c9275c429b783
Sven Seeberg <mail@sven-seeberg.de>
2018-12-11 19:15:45 +0100

    Remove Integreat Wordpress theme
    * The theme is no longer required and interferes with other
      functions like account activation.
    * Fixes #884.

--- wp-content/themes/integreat-webview/templates/breadcrumb.php
+++
@@ -1,9 +0,0 @@
-<nav id="breadcrumb" class="closed ellipsis">
-    <div class="container">
-        <div class="row">
-            <button id="toggleBreadcrumb"><i class="fa fa-plus"></i></button>
-
-            <?php echo custom_breadcrumbs(); ?>
-        </div>
-    </div>
-</nav>
