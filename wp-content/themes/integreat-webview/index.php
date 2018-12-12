563e4237b6bd90770dc8d3a565a9d651233bf1d9
Sven Seeberg <sven@geeq.de>
2018-12-12 15:24:20 +0100

    Fix account activation (#885)
    * Remove URL rewrite for wp-activation.php
    
    * Remove Apache2 mod_rewrite directive for wp-activation.php.
      This file needs to be accessed for account activation.
    
    * Remove Integreat Wordpress theme
    
    * The theme is no longer required and interferes with other
      functions like account activation.
    * Fixes #884.

--- wp-content/themes/integreat-webview/index.php
+++ wp-content/themes/integreat-webview/index.php
@@ -1,25 +1,4 @@
 <?php
 $uri = str_replace("/wordpress","",$_SERVER['REQUEST_URI']);
 header('Location: https://web.integreat-app.de'.$uri, true, 301);
-die();
 get_header(); ?>
-
-    <?php get_template_part('templates/contentHeader'); ?>
-
-    <main>
-        <div class="container">
-            <?php if( $_GET['isite'] == event ): ?>
-                <?php echo do_shortcode('[events_list]'); ?>
-            <?php elseif( is_front_page() ): ?>
-                <?php include get_template_directory() . '/templates/frontPage.php'; ?>
-            <?php else: ?>
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                    <section class="content searchOnSiteContent">
-                        <?php the_content(); ?>
-                    </section>
-                <?php endwhile; endif; ?>
-            <?php endif; ?>
-        </div>
-    </main>
-
-<?php get_footer(); ?>
