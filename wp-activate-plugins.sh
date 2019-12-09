8c7268df5b18c2557b5761c25a9dbffad3351a46
Sven Seeberg <mail@sven-seeberg.de>
2019-12-09 16:22:27 +0100

    Activate all

--- wp-activate-plugins.sh
+++ wp-activate-plugins.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 vendor/wp-cli/wp-cli/bin/wp plugin deactivate --network --all
-for D in `find wp-content/plugins/* -type d -maxdepth 0 -printf "%f\n"`
-do
-    vendor/wp-cli/wp-cli/bin/wp plugin activate --network $D
-done
+vendor/wp-cli/wp-cli/bin/wp plugin activate --network --all
