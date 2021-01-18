bd5f1e342736ab0e818ee90e05cc3d72b190c951
Sven Seeberg <mail@sven-seeberg.de>
2021-01-18 16:01:04 +0100

    FCM: escape line breaks

--- wp-content/plugins/firebase-notifications/classes/database.php
+++ wp-content/plugins/firebase-notifications/classes/database.php
@@ -180,7 +180,7 @@ class FirebaseNotificationsDatabase {
         global $wpdb;
         $request = str_replace( '\"', '&quot;', $request );
         $request = str_replace( "'", "&#39;", $request );
-        $request = str_replace( array('\r\n', '\n'), "<br>", $request );
+        $request = addslashes( $request );
         $query = "INSERT INTO " . $wpdb->prefix . "fcm_messages (sent_message, returned_message) VALUES ('" . $request . "', '" . $answer . "')";
         if( $wpdb->query($query) ) {
             return true;
