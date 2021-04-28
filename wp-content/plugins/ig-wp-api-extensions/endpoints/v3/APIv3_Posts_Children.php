fe8a38d4684c394e461d50b51c34a56d9d99fe9c
Sven Seeberg <mail@sven-seeberg.de>
2021-04-28 15:53:42 +0200

    Change APIv3 children endpoint, fixes WP-43

--- wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
@@ -5,14 +5,10 @@ class APIv3_Posts_Children extends APIv3_Posts_Relatives_Abstract {
 	const ROUTE = 'children';
 
 	public function get_children(WP_REST_Request $request) {
-		$children = (new WP_Query([
-			'post_type' => static::POST_TYPE,
-			'post_status' => 'publish',
-			'post_parent' => $this->get_post($request)->ID,
-			'orderby' => 'menu_order post_title',
-			'order'   => 'ASC',
-			'posts_per_page' => -1,
-		]))->posts;
+		$children = $this->get_posts_recursive(
+			$this->get_post($request)->ID,
+			($request['depth'] ? (int)$request['depth'] : -1)
+		);
 		return array_map([$this, 'prepare'], $children);
 	}
 
