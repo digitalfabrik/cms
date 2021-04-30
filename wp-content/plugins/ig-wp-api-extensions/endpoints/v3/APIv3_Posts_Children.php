704823a7e18ed22edb69e54ae0673bc07d62695e
Sven Seeberg <mail@sven-seeberg.de>
2021-04-30 18:00:13 +0200

    Change APIv3 default children depth

--- wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
@@ -8,7 +8,7 @@ class APIv3_Posts_Children extends APIv3_Posts_Relatives_Abstract {
 		$parameters = $request->get_params();
 		$children = $this->get_posts_recursive(
 			$this->get_post($request)->ID,
-			( array_key_exists( 'depth', $parameters ) ? (int)$parameters['depth'] : -1 )
+			( array_key_exists( 'depth', $parameters ) ? (int)$parameters['depth'] : 1 )
 		);
 		return array_map([$this, 'prepare'], $children);
 	}
