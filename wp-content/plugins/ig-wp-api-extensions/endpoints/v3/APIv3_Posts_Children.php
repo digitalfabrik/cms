db985cfb445f0a57b24b893bfba4969aedcf1758
Sven Seeberg <mail@sven-seeberg.de>
2021-04-30 17:58:42 +0200

    Change APIv3 request parameters

--- wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
@@ -5,9 +5,10 @@ class APIv3_Posts_Children extends APIv3_Posts_Relatives_Abstract {
 	const ROUTE = 'children';
 
 	public function get_children(WP_REST_Request $request) {
+		$parameters = $request->get_params();
 		$children = $this->get_posts_recursive(
 			$this->get_post($request)->ID,
-			( array_key_exists( 'depth', $request ) ? (int)$request['depth'] : -1 )
+			( array_key_exists( 'depth', $parameters ) ? (int)$parameters['depth'] : -1 )
 		);
 		return array_map([$this, 'prepare'], $children);
 	}
