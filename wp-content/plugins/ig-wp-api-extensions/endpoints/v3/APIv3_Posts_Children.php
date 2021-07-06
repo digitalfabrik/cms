ed633174f043fb3a2e569353f7b968af9c3be204
Sven Seeberg <mail@sven-seeberg.de>
2021-07-06 10:04:27 +0200

    Change API tracking
    * Randomize last 16 bit of IP address to prevent
      collisions.
    * Collect original user agents instead of parsing
      web or native strings.

--- wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
+++ wp-content/plugins/ig-wp-api-extensions/endpoints/v3/APIv3_Posts_Children.php
@@ -6,6 +6,8 @@ class APIv3_Posts_Children extends APIv3_Posts_Relatives_Abstract {
 	const POST_TYPE = 'page';
 
 	public function get_children(WP_REST_Request $request) {
+		ig_api_page_tracking( 'Integreat API single page download '.ICL_LANGUAGE_CODE );
+
 		$parameters = $request->get_params();
 		$children = $this->get_posts_recursive(
 			$this->get_post($request)->ID,
